5e75e195dd25aae9e06b90ecbbeca6e45738bf20
James Schnebly <jameschnebly@gmail.com>
2021-11-27 21:33:10 -0800

    Added linkedin and githb links

--- src/App.js
+++ src/App.js
@@ -4,11 +4,34 @@ import "./App.css";
 function App() {
   return (
     <div className="Landing">
-      <div class="heroContents">
+      <div className="heroContents">
         <h1>Hi, I'm James Schnebly</h1>
         <h2>Full Stack Developer</h2>
-        {/* <h3>Javascript + Python Advocate</h3> */}
-        <h5>Website Under Development</h5>
+        <div
+          style={{
+            justifyContent: "center",
+            display: "flex",
+          }}
+        >
+          <a
+            href="https://www.linkedin.com/in/schnebly/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="GitHub-64px.png"
+              alt="Github Icon Link"
+              className="icon"
+            />
+          </a>
+          <a
+            href="https://www.github.com/schnebly"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="linkedin.png" alt="LinkedIn Icon Link" className="icon" />
+          </a>
+        </div>
       </div>
     </div>
   );
